a0245932b2d0c3dfddc36d0381206a7246a0ac68
Autumn-T-T <thomas.autumnalicia@gmail.com>
2026-03-16 09:37:15 -0400

    add movie finder frontend UI with poster cards

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const movieRoutes = require("./routes/movieRoutes")
 
 const app = express()
 
-const PORT = 3001
+app.use(express.static("public"))
 
 app.use("/api", movieRoutes)
+
+const PORT = 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
