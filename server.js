928c9cae1e5ebb6f5e18a2a7385736bddc19068d
Autumn-T-T <thomas.autumnalicia@gmail.com>
2026-03-16 00:23:43 -0400

    create movie routes

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+
+const express = require("express")
+const movieRoutes = require("./routes/movieRoutes")
+
+const app = express()
+
+const PORT = 3001
+
+app.use("/api", movieRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
